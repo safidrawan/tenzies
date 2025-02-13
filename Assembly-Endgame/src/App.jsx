2bc6211f4696974aba4aa0255337dc6991e150fa
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-02-13 13:48:25 +0430

    Keyboard highlightling and correct gussed letter appearing

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -1,24 +1,46 @@
 import { useState } from "react";
+import { clsx } from "clsx";
 import "./App.css";
 import { languages } from "./languages";
 export default function App() {
   const [currentWord, setCurrentWord] = useState("react");
-  
+  const [userGuesses, setUserGuesses] = useState([]);
+  console.log(userGuesses);
+  function updateUserGuesses(letter) {
+    setUserGuesses((prev) => [...prev, letter]);
+  }
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const keyboard = alphabet
-    .split("")
-    .map((letter, index) => <button key={index}>{letter}</button>);
+  const keyboard = alphabet.split("").map((letter, index) => {
+    const isGuessed = userGuesses.includes(letter);
+    const isCorrect = currentWord.includes(letter) && isGuessed;
+    const isWrong = isGuessed && !isCorrect;
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    return (
+      <button
+        className={className}
+        onClick={() => updateUserGuesses(letter)}
+        key={index}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
 
   const wordElement = currentWord
     .split("")
-    .map((letter) => <span key={letter}>{letter.toUpperCase()}</span>);
+    .map((letter) => <span className="word-letters" key={letter}>{userGuesses.includes(letter)? (letter.toUpperCase()):""}</span>);
   return (
     <>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>
           Gues the word in 8 attempts to keep the programming world safe from
-          Assembly
+          Assembly!
         </p>
       </header>
       <section className="game-status">
